2fe8139febce4e57302aa8cbf682e6b158b5559e
x28ccddty5-rgb <x28ccddty5@privaterelay.appleid.com>
2026-06-08 23:17:38 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
+console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL);
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
