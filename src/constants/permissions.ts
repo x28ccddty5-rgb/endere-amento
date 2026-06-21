fae3336e1c66041cb51ae3c3888bc09f80329674
x28ccddty5-rgb <x28ccddty5@privaterelay.appleid.com>
2026-06-20 22:42:22 -0300

    Create permissions.ts

---
+++ src/constants/permissions.ts
@@ -0,0 +1,37 @@
+export const PERMISSIONS = {
+  dashboard: [
+    "administrador",
+    "lideranca",
+    "visualizador"
+  ],
+
+  busca: [
+    "administrador",
+    "lideranca",
+    "apoio",
+    "producao"
+  ],
+
+  lancamento: [
+    "administrador",
+    "lideranca",
+    "apoio",
+    "producao"
+  ],
+
+  divergencias: [
+    "administrador",
+    "lideranca",
+    "apoio",
+    "producao"
+  ],
+
+  baseDados: [
+    "administrador",
+    "lideranca"
+  ],
+
+  usuarios: [
+    "administrador"
+  ]
+};
